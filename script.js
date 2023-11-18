3e858a648cde759fe14bcc20124924e7deca5fe5
Kotob M <realkotob@gmail.com>
2023-11-18 16:02:20 +0300

    Add peerjs connection

---
+++ script.js
@@ -0,0 +1,12 @@
+const peer = new Peer(
+  `${Math.floor(Math.random() * 2 ** 18)
+    .toString(36)
+    .padStart(4, 0)}`,
+  {
+    host: location.hostname,
+    debug: 1,
+    path: "/myapp",
+  },
+);
+
+window.peer = peer;
